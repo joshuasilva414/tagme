60539162f6acaab7217b6b4418731da0356a5310
prosh14 <joshuasilva414@gmail.com>
2023-02-02 16:23:44 -0600

    Project page update

--- src/pages/app/projects/[projectId].tsx
+++ src/pages/app/projects/[projectId].tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Link from "next/link";
 import Dropdown from "@/components/inputs/Dropdown";
 import { Prisma, FieldType, Field, PrismaClient } from "@prisma/client";
+import backArrow from "public/img/backarrow.png";
+import Image from "next/image";
+import { InferGetServerSidePropsType } from "next";
 
 type Props = {
   project: Prisma.ProjectGetPayload<{
@@ -9,69 +12,66 @@ type Props = {
   }>;
 };
 
-const renderSwitch: ((field: Field) => JSX.Element) = (field) => {
-  switch(field.fieldType) {
+const renderSwitch: (fieldInput: Field) => JSX.Element = (fieldInput) => {
+  switch (fieldInput.fieldType) {
     case FieldType.SINGLE_CLASS:
-      return <Dropdown options={field.options} />
+      return <Dropdown options={fieldInput.options} />;
     case FieldType.MULTI_CLASS:
-      return <Dropdown options={field.options} multiclass/>
+      return <Dropdown options={fieldInput.options} multiclass />;
     case FieldType.IMAGE:
-      return <h1>Not Ready Yet</h1>
-    default: return <input type="text" />
+      return <h1>Not Ready Yet</h1>;
+    default:
+      return <input type="text" />;
   }
 };
 
-const SingleTaskView = ({ project }: Props) => {
+const SingleTaskView = ({
+      project,
+    }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
-    
       <div className="flex items-center h-[10vh] p-[10px] border border-black ">
         <Link href="/app/dash">
-          <img src="/img/backarrow.png" alt="back button"/>
+          <Image src={backArrow} width={32} alt="back button" />
         </Link>
       </div>
-      
+
       <div className="grid grid-cols-3 h-[80vh] bg-teal-500">
         {/* first section */}
         <div className=" p-[50px]">
           <div className="">
             <div className="flex-wrap flex-column items-center justify-center">
               <h1 className=" font-calsans text-7xl bold">{project?.name}</h1>
-              <p className=" font-calsans text-3xl ">
-                {project?.task?.description}
-              </p>
+              <p className=" font-calsans text-3xl ">{project?.description}</p>
             </div>
           </div>
         </div>
-      
+
         {/* middle section */}
         <div className="  p-[25px] bg-black object-fill">
           <div className=" h-[100%] flex justify-center items-center bg-white object-fill ">
-            <h1>
-              Example goes here
-            </h1>
+            <h1>Example goes here</h1>
           </div>
         </div>
-        
+
         {/* input section */}
         <div>
-          <div className="flex justify-center items-center h-[50px] bg-white border border-black object-fill">
-            <h1>prompt here</h1>
+          <div className="flex justify-center items-center h-min bg-white border border-black object-fill">
+            <h2>{project.task?.description}</h2>
           </div>
           <div className="flex justify-center items-center p-[25px] ">
             <div className="flex-col">
-              
-              <p>
-                {project.task?.fields.map((taskField) => (
-                  renderSwitch(taskField)
+              <div>
+                {project.task?.fields.map((taskField: Field) => (
+                  <div>
+                    <p>{taskField.prompt}</p>
+                    {renderSwitch(taskField)}
+                  </div>
                 ))}
-              </p>
+              </div>
             </div>
           </div>
         </div>
-        
-        
-        
       </div>
     </>
   );
@@ -79,7 +79,7 @@ const SingleTaskView = ({ project }: Props) => {
 
 export const getServerSideProps = async (context: any) => {
   const { projectId } = context.params;
-  const prisma = new PrismaClient();
+  const prisma = Prisma.PrismaClientFetcher || new PrismaClient();
   const project = await prisma.project.findUnique({ where: { id: projectId } });
   return { props: { project: JSON.parse(JSON.stringify(project)) } };
 };
