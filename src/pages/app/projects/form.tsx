48d1ab4cd169fe39c6957a6409b0d5c419fff488
andrew.28784@gmail.com <NegritoSama>
2023-03-22 16:43:05 -0500

    form

--- src/pages/app/projects/form.tsx
+++ src/pages/app/projects/form.tsx
@@ -88,7 +88,7 @@ function Form() {
                           className="mt-2 block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset-focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           required={true}
                         />
-
+                      <div className='flex'>
                         {/* Shows another task when pressing the add button */}
                         {taskList.length - 1 == index && taskList.length < 5 && 
                         (
@@ -117,6 +117,7 @@ function Form() {
                             <span>remove</span>
                           </button>
                           )}
+                        </div>
 
                       </div>
                     </div>
