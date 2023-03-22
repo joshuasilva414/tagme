40e6defc0628fcee311ce551f86f93a02fccc75a
joshuasilva414 <joshuasilva414@gmail.com>
2023-03-22 17:34:26 -0500

    Co-authored-by: Andrew Magana <NegritoSama@users.noreply.github.com> Co-authored-by: Thomas <thomasg1004@users.noreply.github.com> Co-authored-by: kaciz23 <kaciz23@users.noreply.github.com> My changes are better

--- src/pages/app/dash.tsx
+++ src/pages/app/dash.tsx
@@ -3,24 +3,22 @@ import {
   GetServerSidePropsContext,
   type NextPage,
   InferGetServerSidePropsType,
- } from "next";
+} from "next";
 
 import Link from "next/link";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, Project } from "@prisma/client";
+import { prisma } from "@/server/db";
 import ProjectCard from "@/components/dash/ProjectCard";
-import { getPrismaClient } from "@prisma/client/runtime";
- 
- 
- type Props = {
+
+type Props = {
   projects: Prisma.ProjectGetPayload<{
     select: { [K in keyof Required<Prisma.ProjectSelect>]: true };
   }>[];
- };
- 
- 
- const Dash: NextPage<Props> = ({
-      projects,
-    }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+};
+
+const Dash: NextPage<Props> = ({
+  projects,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div className="bg-white h-screen">
       <div className="w-full max-w-[1200px] mx-auto pt-10 border-b-gray-200 border-b-2 grid grid-cols-2">
@@ -31,15 +29,13 @@ import { getPrismaClient } from "@prisma/client/runtime";
         </div>
         <div className="flex justify-end">
           <button className="bg-slate-800 text-white m-2 rounded-full aspect-square ease-in-out hover:bg-slate-700 font-bold">
-            <Link href="/app/projects/form">
-              +
-            </Link>
+            <Link href="/app/projects/form">+</Link>
           </button>
         </div>
       </div>
       <div className="min-h-screen grid-cols-3 mx-auto max-w-[1200px] pt-5">
         {projects.map(
-          (project) =>
+          (project: Project) =>
             project.active && (
               <ProjectCard
                 key={project.id}
@@ -51,18 +47,15 @@ import { getPrismaClient } from "@prisma/client/runtime";
       </div>
     </div>
   );
- };
- 
- 
- export const getServerSideProps: GetServerSideProps = async (
+};
+
+export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
- ) => {
-  const prisma = new PrismaClient();
+) => {
   const projects = await prisma.project.findMany();
   prisma.$disconnect();
   console.dir(projects);
   return { props: { projects: JSON.parse(JSON.stringify(projects)) } };
- };
- 
- 
- export default Dash; 
+};
+
+export default Dash;
